d2b34c5c1835d3d3dc41e7ab1b756467cfb1413c
Fachry Azaria <fahryazaria@gmail.com>
2023-11-10 05:59:43 +0800

    fix navbar

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
+// function toggleMenu() {
+//   const menu = document.querySelector('.menu-links');
+//   const icon = document.querySelector('.hamburger-icon');
+//   menu.classList.toggle('open');
+//   icon.classList.toggle('open');
 
+// }
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
 }
